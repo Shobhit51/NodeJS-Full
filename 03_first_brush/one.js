b5b89bbf1a27c83ee543ef87863cb5fa3f4b5920
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-19 18:02:05 +0000

    changes

---
+++ 03_first_brush/one.js
@@ -0,0 +1,14 @@
+console.log("test")
+
+// run -> in terminal -> node filename
+
+
+
+
+//window does not work in node because it related to browser 
+console.log(window);
+
+
+//npm ->node package manager
+
+//command npm init for config  json file
