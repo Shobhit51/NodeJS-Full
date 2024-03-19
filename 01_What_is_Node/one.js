3277f8b7db39e1378b5b311b6d408e798acc2558
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-19 17:50:32 +0000

    changes

---
+++ 01_What_is_Node/one.js
@@ -0,0 +1,14 @@
+//we can not execute js outside the browser.
+
+//every browser have a unique engine
+
+//v8 is most popular (chrome)
+
+//a developer ryan dahl mix the v8 engine ans c++ and made nodejs
+
+//js can talk to native machine because of c++
+
+//you can create webservers in js language
+
+//Node js a runtime  Env. for javascript
+
